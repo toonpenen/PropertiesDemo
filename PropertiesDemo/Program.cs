dace8e6f1252e04f7d1b1dbfa26afb5a908625f2
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-16 09:43:50 +0100

    Add project files.

---
+++ PropertiesDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    public class Student
+    {
+        private string _name;
+
+        public void SetName(string name)
+        {
+            if (!String.IsNullOrEmpty(name))
+            {
+                this._name = name;
+            }
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student._name = " ";
+
+            Console.WriteLine(student.name);
+        }
+    }
+}
